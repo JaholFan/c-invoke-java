83639bc5ee894ed9fd34119114f37d5b998b754a
Jahol Fan <fjh@lonbon.com>
2020-06-14 12:22:35 +0800

    c invoke none static java method

--- Test.java
+++ Test.java
@@ -3,4 +3,12 @@ public class Test{
     public static void main(String[] args){
         System.out.println("hello world");
     }
+    public int my_print(String str){
+        System.out.println(""+str);
+        return 0;
+    }
+    public int my_print(int val){
+        System.out.println("data="+val);
+        return 0;
+    }
 }
--- c_test.c
+++ c_test.c
@@ -18,7 +18,9 @@ int main(int argc,char**argv)
 	JavaVM* jvm;
 	JNIEnv* env;
 	jclass cls;
-	jmethodID mid;
+	jmethodID mid,cid,id_1;
+    jstring jstr;
+    jobject jobj;
 	int ret = 0;
     /* 1. create java virtual machine*/
 	ret = create_vm(&jvm,&env);
@@ -33,7 +35,8 @@ int main(int argc,char**argv)
 		ret = -1;
 		goto err;
 	}
-    /* 3. intantiate object*/
+    /************example 1:call static method*************/
+    /* 3. intantiate object(skip this step)*/
     /* 4. call method 
      * 4.1 get method
      * 4.2 create parameter
@@ -47,6 +50,47 @@ int main(int argc,char**argv)
 		goto err;
 	}
 	(*env)->CallStaticVoidMethod(env, cls, mid, NULL);
+    /************examle 2:call none static method*********/
+    /* 3. intantiate object
+     * 3.1 get constructor method
+     * 3.2 create parameter
+     * 3.3 NewObject
+     */
+	cid = (*env)->GetMethodID(env, cls, "<init>",
+									"()V");
+	if (cid == NULL) {
+		ret = -3;
+		printf("Can't get constructor method\n");
+		goto err;
+	}
+    
+    jobj = (*env)->NewObject(env, cls, cid); 
+	if (jobj == NULL) {
+		ret = -4;
+		printf("Can't new object\n");
+		goto err;
+	}
+    
+    /* 4. call method 
+     * 4.1 get method
+     * 4.2 create parameter
+     * 4.3 call method
+     */
+	id_1 = (*env)->GetMethodID(env, cls, "my_print",
+									 "(Ljava/lang/String;)I");
+	if (id_1 == NULL) {
+		ret = -5;
+		printf("Can't get method:my_print\n");
+		goto err;
+	}
+    jstr = (*env)->NewStringUTF(env, "Jahol Fan");
+	ret = (*env)->CallIntMethod(env, jobj, id_1, jstr);
+    if(ret){
+		ret = -6;
+		printf("Can't call method:my_print\n");
+		goto err;
+        
+    }
 err:
 	(*jvm)->DestroyJavaVM(jvm);
 _return:
